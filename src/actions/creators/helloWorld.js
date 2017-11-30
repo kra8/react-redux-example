5245faf2daebfc0e83e9c34046b598d5ac92137e
K.Asai <kra.enmn0618.swift@softbank.ne.jp>
2017-11-30 13:12:21 +0900

    Write about curring

--- src/actions/creators/helloWorld.js
+++ src/actions/creators/helloWorld.js
@@ -22,23 +22,35 @@ export const fetchHelloWorldFail = () => ({
 //
 // thunk middleware supported
 //
-export const fetchTasks = () => {
-  return dispatch => {
-    dispatch(fetchHelloWorldStart())
+// curryingという、複数の引数の関数を一つの引数の関数に減らす方法があります。
+// おそらく、ここでネストした高階関数を定義しないといけないのは、
+// ミドルウェアによって、カリー化されているから？
+// この場合、uncurringでカレー化の逆の操作を用いて高階関数のネストを防げます。
+// https://blog.benestudio.co/currying-in-javascript-es6-540d2ad09400
+//
+// export const fetchTasks = () => {
+//   return dispatch => {
+//     dispatch(fetchHelloWorldStart())
+//
+//     return fetch(uri)
+//       .then(response => response.json())
+//       .then(message => dispatch(fetchHelloWorldSuccess(message)))
+//       .catch(() => dispatch(fetchHelloWorldFail()))
+//   }
+// }
+export const fetchTasks = () => dispatch => {
+  dispatch(fetchHelloWorldStart())
 
-    return fetch(uri)
-      .then(response => response.json())
-      .then(message => dispatch(fetchHelloWorldSuccess(message)))
-      .catch(() => dispatch(fetchHelloWorldFail()))
-  }
+  return fetch(uri)
+    .then(response => response.json())
+    .then(message => dispatch(fetchHelloWorldSuccess(message)))
+    .catch(() => dispatch(fetchHelloWorldFail()))
 }
 
-export const getHelloWorldIfNeeded = () => {
-  return (dispatch, getState) => {
-    if (getState().helloWorld.isFetching) {
-      return Promise.resolve()
-    } else {
-      return dispatch(fetchTasks())
-    }
+export const getHelloWorldIfNeeded = () => (dispatch, getState) => {
+  if (getState().helloWorld.isFetching) {
+    return Promise.resolve()
+  } else {
+    return dispatch(fetchTasks())
   }
 }
